624b6c43da5deb3e0836c53de81e21189a87ad3b
Micheal Taylor <bubthegreat@gmail.com>
2019-10-06 01:11:57 -0600

    Updated for testing to work in CI/CD for app

--- {{cookiecutter.project_slug}}/{{cookiecutter.angular_slug}}/src/app/app.component.spec.ts
+++ {{cookiecutter.project_slug}}/{{cookiecutter.angular_slug}}/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('{{cookiecutter.project_name}} app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('{{cookiecutter.project_name}}');
   });
 });
