30c1490b5a771549c35ecb1b98ea12d4eadd40b6
Micheal Taylor <bubthegreat@gmail.com>
2019-10-06 00:56:59 -0600

    Added github actions and a basic test.

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.python_slug}}/tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import tests for {{ cookiecutter.python_slug }}."""
+
+import pytest
+
+def test_import():
+    """Test that we can import the installed module successfully."""
+    import {{ cookiecutter.python_slug }}
