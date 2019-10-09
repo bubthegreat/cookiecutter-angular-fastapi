00715d05bfbda4f1580f94ad84910d954de57234
Micheal Taylor <bubthegreat@gmail.com>
2019-10-08 21:59:01 -0600

    Updated to F string for pre-create hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,7 +7,6 @@ MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{ cookiecutter.python_slug }}'
 
 if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: %s is not a valid Python module name!' % module_name)
-
-    # exits with status 1 to indicate failure
+    print(f'ERROR: {module_name} is not a valid Python module name!')
+    # Exit 1 to fail
     sys.exit(1)
